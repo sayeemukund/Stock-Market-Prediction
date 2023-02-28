dd80f23402d455b3623850c598581011cc60b4d3
Sayee Mukund <100687774+sayeemukund@users.noreply.github.com>
2023-02-28 11:08:09 +0530

    Update stacked_lstm.py

--- stacked_lstm.py
+++ stacked_lstm.py
@@ -86,15 +86,19 @@ try:
             df = load_data(selected_stock1)
             stock_name = data.loc[data['Symbol'].str.upper() == selected_stock, 'Name'].values[0]
             if selected_stock1 in data['Symbol'].str.upper().values:
-                stock_name = data.loc[data['Symbol'].str.upper() == selected_stock1, 'Name'].values[0]
-                st.header(f'The name of the stock with symbol {selected_stock1} is {stock_name}.')
-            else:
-                st.write(f'Stock symbol {selected_stock} not found.')
-            if st.button("Submit"):
-                data_load_state = st.text('Loading data...')
-                st.write("You have entered", selected_stock)
-                st.experimental_data_editor(df)  
-                data_load_state.text('Loading data... done!')
+                if st.button("Submit"):
+                        stock_name = data.loc[data['Symbol'].str.upper() == selected_stock1, 'Name'].values[0]
+                        st.header(f'The name of the stock with symbol {selected_stock1} is {stock_name}.')
+                        st.write("You have entered", selected_stock)
+                        st.experimental_data_editor(df)  
+                        data_load_state.text('Loading data... done!')
+                else:
+                        st.write(f'Stock symbol {selected_stock} not found.')
+            
+#             data_load_state = st.text('Loading data...')
+#             st.write("You have entered", selected_stock)
+#             st.experimental_data_editor(df)  
+#             data_load_state.text('Loading data... done!')
 
 
         df1=df.copy()
