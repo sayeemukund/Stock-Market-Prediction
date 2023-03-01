a8827e5879eef2463b3067f7963d2cd161830e66
Sayee Mukund <100687774+sayeemukund@users.noreply.github.com>
2023-03-01 16:45:31 +0530

    Update about.py

--- pages/about.py
+++ pages/about.py
@@ -5,7 +5,6 @@ import numpy
 st.write('hello')
 
 st.set_page_config(page_title="Stock Prediction", page_icon=":chart_with_upwards_trend:")
-
 selected=option_menu(
         menu_title='Stock Market Prediction',
         options= ['Introduction','Procedure'],
@@ -16,12 +15,10 @@ selected=option_menu(
         "container": {"padding": "0!important", "background-color": "grey"},
         "icon": {"color": "black", "font-size": "30px"}, 
         "nav-link": {"font-size": "20px", "text-align": "left", "margin":"0px", "--hover-color": "white"},
-        "nav-link-selected": {"background-color": "#13A88A"},
-    }
-    )
+        "nav-link-selected": {"background-color": "#13A88A"},})
 
 if selected=='Introduction':
-  st.title('Stock Makert Prediction.')
+        st.title('Stock Makert Prediction.')
   st.header('Please Note: This app is created just for educational purpose.')
   st.write('-    Stock market prediction using stacked LSTM is a popular application of deep learning in finance..Several LSTM layers make up a stacked LSTM model, a kind of recurrent neural network (RNN). A series of historical stock market data, including daily stock prices, trading volume serves as the input of the model.The output is a forecast of future stock price movement.')
   st.write('-    The stacked LSTM model employs a number of levels of LSTM cells, each layer made up of a number of LSTM units. The first layer of the model receives the input data, while the subsequent layers receive the output of the previous layer as input. When each layer can capture more abstract and high-level representations of the input, the model is able to learn increasingly complicated patterns in the input data.')
@@ -30,7 +27,7 @@ if selected=='Introduction':
   st.write('-    It is crucial to remember that there is no assurance that stock market predictions will be accurate, even if they might be valuable for making wise financial decisions. The stock market is complicated and affected by a variety of unpredictable circumstances, such as world events, political unrest, and natural disasters.')
 
 if selected=='Procedure':
-  st.header('Steps Involved.')
+        st.header('Steps Involved.')
   st.write('-    A text input is created for entering your desired stock symbol(PS: CASE SENSITIVE).')
   st.write('-    Data retreived from yfinance library ranging from 1st January 2021 till today.')
   st.write('-    Plotting of graphs which includes:')
